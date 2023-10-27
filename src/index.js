f5f6692c23f004d3bd8aaf177280423e4ccfa5ff
bonny-art <svitlana.otenko@gmail.com>
2023-10-28 00:44:11 +0300

    adds axios

--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
-// import axios from 'axios';
 import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-// axios.defaults.headers.common['x-api-key'] = API_KEY;
-
-// Make a request for a user with a given ID
-// axios
-//   .get('/user?ID=12345')
-//   .then(function (response) {
-//     // handle success
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-//   .finally(function () {
-//     // always executed
-//   });
 
 const elem = {
   selector: document.querySelector('.breed-select'),
@@ -36,14 +18,14 @@ elem.error.classList.add('js-hide');
 elem.selector.addEventListener('change', handlerFindCat);
 
 fetchBreeds()
-  .then(data => {
+  .then(({ data }) => {
     elem.selector.innerHTML =
       `<option data-placeholder="true"></option>` + createSelectMarkup(data);
 
     new SlimSelect({
       select: elem.selector,
       settings: {
-        placeholderText: 'Select a breed of a cat',
+        placeholderText: 'Select a cat breed',
       },
     });
 
@@ -51,7 +33,6 @@ fetchBreeds()
   })
   .catch(err => {
     Notify.failure('Oops! Something went wrong! Try reloading the page!');
-    console.log(err);
   })
   .finally(() => {
     elem.loader.classList.add('js-hide');
@@ -69,7 +50,7 @@ function handlerFindCat(evt) {
   const id = evt.currentTarget.value;
 
   fetchCatByBreed(id)
-    .then(data => {
+    .then(({ data }) => {
       const { url, breeds } = data[0];
       const { name, description, temperament } = breeds[0];
 
@@ -80,8 +61,6 @@ function handlerFindCat(evt) {
     })
     .catch(err => {
       Notify.failure('Oops! Something went wrong! Try reloading the page!');
-
-      console.log(err);
     })
     .finally(() => {
       elem.loader.classList.add('js-hide');
@@ -102,7 +81,3 @@ function createCatMarkup(url, name, description, temperament) {
         </div>
       `;
 }
-
-// TODO Add axios instead of fetch
-
-// TODO Move loader clothing to finally?
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,32 +1,45 @@
+import axios from 'axios';
+
+const MAIN_URL = 'https://api.thecatapi.com/v1';
 const API_KEY =
   'live_ANXFBEcIM0yKus5fb58clof0MJOs4zLea5inllroiU2Lrmx56ZK8Y8l86jVTvJjp';
-const MAIN_URL = 'https://api.thecatapi.com/v1';
+
+axios.defaults.headers.common['x-api-key'] = API_KEY;
 
 function fetchBreeds() {
-  return fetch(`${MAIN_URL}/breeds`).then(resp => {
-    if (!resp.ok) {
-      throw new Error(resp.statusText || 'Some error happend');
-    }
-    return resp.json();
-  });
+  //   return fetch(`${MAIN_URL}/breeds`).then(resp => {
+  //     if (!resp.ok) {
+  //       throw new Error(resp.statusText || 'Some error happend');
+  //     }
+  //     return resp.json();
+
+  // });
+
+  return axios.get(`${MAIN_URL}/breeds`);
 }
 
 function fetchCatByBreed(breedId) {
-  const Params = new URLSearchParams({
-    breed_ids: breedId,
-  });
+  //   const Params = new URLSearchParams({
+  //     breed_ids: breedId,
+  //   });
 
-  const options = {
-    headers: {
-      'x-api-key': API_KEY,
+  //   const options = {
+  //     headers: {
+  //       'x-api-key': API_KEY,
+  //     },
+  //   };
+
+  //   return fetch(`${MAIN_URL}/images/search?${Params}`, options).then(resp => {
+  //     if (!resp.ok) {
+  //       throw new Error(resp.statusText || 'Some error happend');
+  //     }
+  //     return resp.json();
+  //   });
+
+  return axios.get(`${MAIN_URL}/images/search`, {
+    params: {
+      breed_ids: breedId,
     },
-  };
-
-  return fetch(`${MAIN_URL}/images/search?${Params}`, options).then(resp => {
-    if (!resp.ok) {
-      throw new Error(resp.statusText || 'Some error happend');
-    }
-    return resp.json();
   });
 }
 
